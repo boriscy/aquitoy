3ada5681e3a066c28fbdbe98d112f2d69595baf0
Boris Barroso <boris@boris-compu.(none)>
2009-10-06 16:56:53 -0400

    Registro bien testeado

--- app/models/registro.rb
+++ app/models/registro.rb
@@ -3,18 +3,20 @@ class Registro < ActiveRecord::Base
   # callbacks
   before_create :adicionar_entrada_salida
 
-  cattr_reader :entradas, :salidas
+  cattr_reader :entradas, :salidas, :dia
+  cattr_accessor :rangos_por_defecto
   
   belongs_to :usuario
 
   attr_accessor :ci
 
-
-  # Adiciona las horas de entrada o salida
   def adicionar_entrada_salida
     salida = true
     ejecutado = false
-    self.class.definir_horarios() if @@entradas.nil?
+    if @@dia.nil? or Date.today != @@dia
+      self.class.flush_cache(:definir_horarios)
+      self.class.definir_horarios(nil, Date.today.to_s)
+    end
 
     @@entradas.each do |rango|
       if rango.include?(Time.zone.now)
@@ -69,12 +71,21 @@ private
   class << self
     extend ActiveSupport::Memoizable 
 
-    def definir_horarios(arr = nil)
-      arr ||= [["08:20", "10:00"], ["14:20", "16:00"]]
-      @@entradas = crear_rangos_entradas(arr)
-      @@salidas = crear_rangos_salidas(arr)
+    def definir_horarios(arr = nil, dia = Date.today.to_s)
+      crear_rangos_por_defecto() if @@rangos_por_defecto.nil? # Cuidado, hay que llamar la la funcion usando ()
+      @@dia = Date.today
+      arr ||= @@rangos_por_defecto
+      @@entradas = crear_rangos_entradas(arr, dia)
+      @@salidas = crear_rangos_salidas(arr, dia)
+    end
+
+    def crear_rangos_por_defecto
+      @@rangos_por_defecto = [["08:20", "10:00"], ["14:20", "16:00"]]
     end
     
+    # Memoriza el rango como un cache
+    memoize :definir_horarios
+
     def entradas
       @@entradas
     end
@@ -83,8 +94,6 @@ private
       @@salidas
     end
 
-    # Memoriza el rango como un cache
-    memoize :definir_horarios
 
   #####################################
   # Metodos privados
@@ -92,11 +101,10 @@ private
     # Crear rangos validos para horas de entrada y salida
     # @param array horas
     # ==== Ejemplo:
-    #   crear_rangos_entradas([["08:00", "09:00"],["14:00", "15:00"]])
-    def crear_rangos_entradas(horas)
-      d = Date.today.to_s
+    #   crear_rangos_entradas([["08:00", "09:00"],["14:00", "15:00"]], "2009-10-05")
+    def crear_rangos_entradas(horas, dia)
       horas.inject([]){|arr, v|
-        arr << ( Time.zone.parse("#{d} #{v[0]}" )..Time.zone.parse("#{d} #{v[1]}") )
+        arr << ( Time.zone.parse("#{dia} #{v[0]}" )..Time.zone.parse("#{dia} #{v[1]}") )
         arr
       }
     end
@@ -104,16 +112,15 @@ private
     # Retorna un array con las horas de salida basado en las horas de entrada
     # @param array horas
     # ==== Ejemplo:
-    #   crear_rangos_salidas([["08:00", "09:00"],["14:00", "15:00"]])
-    def crear_rangos_salidas(horas)
-      d = Date.today.to_s
+    #   crear_rangos_salidas([["08:00", "09:00"],["14:00", "15:00"]], "2009-10-05")
+    def crear_rangos_salidas(horas, dia)
       ret = []
-      ingreso_dia_siguiente = Time.zone.parse("#{d} #{horas[0][0]}") + 1.day - 1.second
+      ingreso_dia_siguiente = Time.zone.parse("#{dia} #{horas[0][0]}") + 1.day - 1.second
 
       horas.each_index do |i|
-        hora_salida_inicial = Time.zone.parse("#{d} #{horas[i][1]}") + 1
+        hora_salida_inicial = Time.zone.parse("#{dia} #{horas[i][1]}") + 1
         if horas[i+1]
-          hora_salida_final = Time.zone.parse("#{d} #{horas[i+1][0]}") - 1
+          hora_salida_final = Time.zone.parse("#{dia} #{horas[i+1][0]}") - 1
           ret << (hora_salida_inicial..hora_salida_final)
         else
           ret << (hora_salida_inicial..ingreso_dia_siguiente)
