39749ab152955afe9ae21feaa6e12c934326be84
Boris Barroso <boris@boris-compu.(none)>
2009-10-07 14:14:57 -0400

    Mejora de presentación

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,14 @@ module ApplicationHelper
     messages
   end
 
+  def title(page_title, show_title = true)
+    @content_for_title = page_title.to_s
+    @show_title = show_title
+  end
+  
+  def show_title?
+    @show_title
+  end
+
+
 end
